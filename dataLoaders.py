61c29f62e08c5a24bb5f00979f721ff1bbf9c81e
Ionelia Buzatu <buzatuionelia@gmail.com>
2019-04-25 17:32:20 +0200

    added validation

--- dataLoaders.py
+++ dataLoaders.py
@@ -3,10 +3,11 @@ import hickle as hkl
 import pandas as pd
 import numpy as np
 import torch.utils.data
+from torch.utils.data.sampler import SubsetRandomSampler
 from config import train_geno, train_pheno, test_geno, test_pheno
 
+
 def train_data():
-    # data loading: train and test
     trainX = hkl.load(train_geno)
     trainX = torch.from_numpy(trainX).float()
     trainY = pd.read_csv(train_pheno, sep="\t")
@@ -20,7 +21,7 @@ def train_data():
 
     # normalization for x and y
     trainX -= trainX.mean(dim=0)
-    trainX /= trainX.var(dim=0) + 1e-10 # a small number is added to prevent nan when sd == 0
+    trainX /= trainX.var(dim=0) + 1e-10  # a small number is added to prevent nan when sd == 0
     trainY -= trainY.mean()
     trainY /= trainY.var()
     concatXY -= concatXY.mean(dim=0)
@@ -51,8 +52,61 @@ def test_data():
     # testY /= testY.var()
 
     test = torch.utils.data.TensorDataset(testX, testY)
-    test_loader = torch.utils.data.DataLoader(test, batch_size=1, shuffle=False, drop_last=True)
-    return test_loader, x_mean, x_var, y_mean, y_var
+    test_loader = torch.utils.data.DataLoader(test, batch_size=50, shuffle=False, drop_last=True)
+
+    return test_loader, x_mean, x_var, y_mean, y_var, len(testY)
+
+
+def train_val_test():
+    """
+    :return: trainining, validation and test sets, of length 0.8, 0.1, 0.1
+    """
+    trainX = hkl.load(train_geno)
+    trainX = torch.from_numpy(trainX).float()
+    trainY = pd.read_csv(train_pheno, sep="\t")
+    trainY = torch.tensor(trainY["f.4079.0.0"].values).float().unsqueeze(-1)
+    y_mean = trainY.mean()
+    y_var = trainY.var()
+    concatXY = np.column_stack((trainX, trainY))
+    concatXY = torch.from_numpy(concatXY)
+    x_mean = trainX.mean(dim=0)
+    x_var = trainX.var(dim=0)
+
+    # normalization for x and y
+    trainX -= trainX.mean(dim=0)
+    trainX /= trainX.var(dim=0) + 1e-10  # a small number is added to prevent nan when sd == 0
+    trainY -= trainY.mean()
+    trainY /= trainY.var()
+    concatXY -= concatXY.mean(dim=0)
+    concatXY /= concatXY.var(dim=0)
+
+    dataset = torch.utils.data.TensorDataset(trainX, trainY)
+    # TODO: think of a way to import only once batch_size evrywhere
+    # train_loader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=False, drop_last=True)
+
+    validation_split = 0.8
+    dataset_size = len(dataset)
+    indices = list(range(dataset_size))
+    split = int(np.floor(validation_split * dataset_size))
+    if 1:
+        np.random.seed(1337)
+        np.random.shuffle(indices)
+    test_indices, valid_indices, train_indices = indices[split // 2:], indices[:split // 2], indices[:split]
+    train_sampler = SubsetRandomSampler(train_indices)
+    valid_sampler = SubsetRandomSampler(valid_indices)
+    test_sampler = SubsetRandomSampler(test_indices)
+
+    train_loader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=False,
+                                               sampler=train_sampler, drop_last=True)
+    validation_loader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=False,
+                                                    sampler=valid_sampler, drop_last=True)
+
+    test_loader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=False,
+                                              sampler=test_sampler, drop_last=True)
+
+    return train_loader, validation_loader, trainX.shape[1], trainY.shape[1], x_mean, x_var, y_mean, y_var
+
 
 train_data()
 test_data()
+train_val_test()
