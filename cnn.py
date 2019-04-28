3564a74f5c0f0bf341e85ddd474e409d727234e7
Ionelia Buzatu <buzatuionelia@gmail.com>
2019-04-28 19:22:05 +0200

    still overfitting

--- cnn.py
+++ cnn.py
@@ -13,7 +13,7 @@ from dataLoaders import train_data, test_data, train_val_test
 from torch.utils import data
 
 writer = tensorboardX.SummaryWriter()
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu') # Device configuration
 
 # 3000 * 6000
 class CNN(nn.Module):
@@ -23,12 +23,15 @@ class CNN(nn.Module):
         # (in_channels, out_channels, kernel_size, stride, padding)
         self.kernel_size = 3
         self.paddings = self.kernel_size // 2
+        self.stride = 3
+        self.dropout = nn.Dropout(p=0.5)
 
         self.conv1 = nn.Conv1d(1, 4, kernel_size=self.kernel_size, stride=5, padding=self.paddings)
         self.pool = nn.MaxPool1d(kernel_size=self.kernel_size, stride=3, padding=self.paddings)
-        self.conv2 = nn.Conv1d(4, 16, kernel_size=self.kernel_size, stride=3, padding=self.paddings)
+        self.conv2 = nn.Conv1d(4, 16, kernel_size=self.kernel_size, stride=self.stride, padding=self.paddings)
         self.fc1 = nn.Linear(2144, 200)
         self.fc2 = nn.Linear(200, 1)
+
 
     def forward(self, x):
         # print("oroginal x " + str(x.size()))
@@ -39,6 +42,7 @@ class CNN(nn.Module):
         x = self.pool(x)
         # print("after pool " + str(x.size()))
         x = torch.relu_(self.conv2(x))
+        x = self.dropout(x)
         # x = self.pool(x)
         # print("after 2nd conv " + str(x.size()))
         x = x.view(x.size(0), -1)
@@ -61,23 +65,21 @@ net = net.to(device)
 epochs = 100
 learning_rate = 0.0001
 
-# train_loader, x_features, y_features, x_mean, x_var, y_mean, y_var = train_data()
+train_loader, x_features, y_features, x_mean, x_var, y_mean, y_var = train_data()
+validation_loader, x_mean, x_var, y_mean, y_var, lenY_validation = test_data()
+
 # train, validation and test sets with length
 # train_set, val_set, test_set = data.random_split(train_loader, (2100, 900, 0))
 
 
-train_loader, validation_loader, x_features, y_features, x_mean, x_var, y_mean, y_var = train_val_test()
+# train_loader, validation_loader, x_features, y_features, x_mean, x_var, y_mean, y_var = train_val_test()
 
 # alternative
 from sklearn.model_selection import train_test_split
-
 # X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.2, random_state=42)
 
-
-# Loss functions
-# loss = nn.CrossEntropyLoss()
-criterion = nn.SmoothL1Loss()
-mean = nn.MSELoss()
+# criterion = nn.SmoothL1Loss()
+criterion = nn.MSELoss()
 
 # Optimizer
 optimizer = optim.Adam(net.parameters(), lr=learning_rate)
@@ -88,6 +90,7 @@ for epoch in range(epochs):
     train_loss = 0  # sum all the losses and divide by the number of batches
     deno_loss = 0
     for i, data in enumerate(train_loader):
+        net.train()
         train_step = epoch * len(train_loader) + i
         inputs, labels = data
         inputs, labels = inputs.to(device), labels.to(device)
@@ -114,7 +117,7 @@ for epoch in range(epochs):
         writer.add_scalar('/Step/Loss', l.item(), train_step)
 
     # low variance if the gap between the train and validation loss is really close
-    net.eval()
+
     val_loss = 0
     for i, data in enumerate(validation_loader):
         step_val = epoch * len(train_loader) + i
@@ -130,19 +133,15 @@ for epoch in range(epochs):
     # writer.add_scalar('Epoch/TrainFc/deno', deno_loss, epoch)
     writer.add_scalar('Epoch/Loss', train_loss, epoch)
     writer.add_scalar('Epoch/Loss', val_loss, epoch)
-    print('Epoch:  %d | Train Loss: %.4f  | Val Loss: %.4f' % (epoch + 1, train_loss, val_loss))
+    print('Epoch:  %d/100 | Train Loss: %.4f  | Val Loss: %.4f' % (epoch + 1, train_loss, val_loss))
 
-
-if __name__ == "__main__":
-    pass
 
 """
-
 try:
-    net.load_state_dict(torch.load('modelWeights/paramsCNN.ckpt'))
+    net.load_state_dict(torch.load('modelWeights/paramsCNN60k.ckpt'))
 except FileNotFoundError:
     # training(model, train_loader, optimizer)
-    torch.save(net.state_dict(), 'modelweights/paramsCNN.ckpt')
-
-
+    torch.save(net.state_dict(), 'modelweights/paramsCNN60k.ckpt')
 """
+
+
